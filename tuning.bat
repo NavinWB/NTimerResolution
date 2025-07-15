5c32fe05d1aee7a79c4743c48489278cb582da3e
NavinT <ksagix01@gmail.com>
2025-07-15 01:10:56 -0600

    Delete tuning.bat

--- tuning.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-bcdedit /set useplatformtick yes
-bcdedit /set disabledynamictick yes
-bcdedit /deletevalue useplatformclock
-echo.
-echo Ajustes aplicados. Reinicia el equipo para que surtan efecto.
-pause
